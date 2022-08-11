b308b75b655cc59b9e68ec4ee58508ac28e4e606
Eglany Junior <contas.eglanyjr@gmail.com>
2022-08-11 19:17:24 -0400

    :heavy_plus_sign: intall material ui, eslint

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import './App.css';
+import { useEffect, useState } from 'react';
+
+function App() {
+  const [films, setFilms] = useState([{}]);
+
+  const fetchGhibli = async () => {
+    const api = await fetch('https://ghibliapi.herokuapp.com/films');
+    const data = await api.json();
+    setFilms(data);
+  };
+
+  useEffect(() => {
+    fetchGhibli();
+  }, []);
+
+  return (
+    <div className="App">
+      {films.map(({ title, image, description }) => (
+        <div id="card">
+          <img src={image} alt="" />
+          <h1>{title}</h1>
+          <p>{description}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default App;
