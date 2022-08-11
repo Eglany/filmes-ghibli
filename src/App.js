aa52bc9f31f931736e75a8e29bd484d1f2a73da4
Eglany Junior <contas.eglanyjr@gmail.com>
2022-08-11 18:54:23 -0400

    start project
    feat: render films in screen

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [films, setFilms] = useState([{}])
+  
+  const fetchGhibli = async () => {
+    const api = await fetch("https://ghibliapi.herokuapp.com/films");
+    const data = await api.json();
+    setFilms(data)
+  }
+
+  useEffect(() => {
+    fetchGhibli()
+  },[]);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {films.map(({title,image, description}) => (
+        <div id='card'>
+            <img src={image} alt="" />
+            <h1>{title}</h1>
+            <p>{description}</p>
+        </div>
+      ))}
     </div>
   );
 }
